95e3a061f38fc4e246ea156b593d67586c25260e
sujaljain272004 <160271734+sujaljain272004@users.noreply.github.com>
2025-08-16 16:16:02 +0530

    Forget Password Implemented and Pagination/Filtering Done !!

--- DivineSpark/src/main/java/com/DivineSpark/repository/BookingRepository.java
+++ DivineSpark/src/main/java/com/DivineSpark/repository/BookingRepository.java
@@ -2,6 +2,7 @@ package com.DivineSpark.repository;
 
 import com.DivineSpark.model.Session;
 import com.DivineSpark.model.SessionBooking;
+import com.DivineSpark.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -13,5 +14,7 @@ public interface BookingRepository extends JpaRepository<SessionBooking,Long> {
     boolean existsBySessionAndUserId(Session session, Long userId);
 
     Optional<SessionBooking> findByJoinToken(String joinToken);
+
+    Optional<SessionBooking> findByUserAndSession(User user, Session session);
 
 }
