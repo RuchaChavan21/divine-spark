46b1f530f8e8a99db93b8710410441bea2041b81
sujaljain272004 <160271734+sujaljain272004@users.noreply.github.com>
2025-09-03 13:13:23 +0530

    CORS Added !

--- DivineSpark/src/main/java/com/DivineSpark/repository/UserRepository.java
+++ DivineSpark/src/main/java/com/DivineSpark/repository/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface    UserRepository extends JpaRepository<User, Long> {
 
     //for login and checking user by email
     Optional<User> findByEmail(String email);
